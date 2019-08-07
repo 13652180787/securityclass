a9dc0847fd3f7b34efd82372119d03e93ee0c6cc
xhp <601362529@qq.com>
2019-08-07 09:18:52 +0800

    Initial commit

---
+++ securityday3/src/main/java/cn/com/taiji/security/securityday3/web/DemoController.java
@@ -0,0 +1,37 @@
+package cn.com.taiji.security.securityday3.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    private Logger logger = LoggerFactory.getLogger(DemoController.class);
+
+
+//    @GetMapping("/")
+//    public String demo1() {
+//        return "Hello World.This is my first Spring Security controller demo!";
+//    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "return admin";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "return user";
+    }
+
+    @GetMapping("/a")
+    public String a() {
+        return "return a";
+    }
+
+    @GetMapping("/b")
+    public String b() {
+        return "return b";
+    }
+}
